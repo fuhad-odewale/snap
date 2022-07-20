c9c691b99ac9b15871f5ed5dfdfdd1cb2febd682
fuhad-odewale <94055903+fuhad-odewale@users.noreply.github.com>
2022-07-20 12:37:38 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -5,7 +5,8 @@ let dropel = document.getElementsByClassName(dropdown)
 
 function drop() {
     let dorp = "drop"
-    fall.innertext = "  <div class="dropdown">
+    fall.innertext = `
+    <div class="dropdown">
     <span>features <img src="images/icon-arrow-down.svg" alt=""></span> 
     <div class="dropdowncontent">
         <a href="#"> 
@@ -32,5 +33,30 @@ function drop() {
                <span class="dpc">planning</span> 
             </p>
         </a>
-    </div>";
+    </div>
+</div>
+
+<div class="dropdown">
+    <span>company <img src="images/icon-arrow-down.svg" alt=""></span> 
+    <div class="dropdowncontent">
+        <a href="#"> 
+            <p>history</p>
+        </a>
+        <a href="#"> 
+            <p>our team</p>
+        </a>
+        <a href="#"> 
+            <p>blog</p>
+        </a>
+    </div>
+</div>
+<ul>
+<li><a href="#">carees</a></li>
+<li><a href="#">about</a></li>
+</ul>
+<div class="menu">
+<a href="#">Login</a>
+<a href="#"><button>register</button></a>
+</div>`
+
 }
